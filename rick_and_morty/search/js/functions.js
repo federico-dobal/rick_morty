bc4fc09e1cb7a318fbd51b2ad206edbe7906a5e0
Federico Dobal <fede.dobal@Federicos-MacBook-Pro.local>
2021-02-13 14:33:59 -0300

    Clean code and set up requirements

--- rick_and_morty/search/js/functions.js
+++ rick_and_morty/search/js/functions.js
@@ -3,44 +3,32 @@
 */
 function update_search_results_div(value, products) {
 
-  console.log('update', value);
-
   let html = '<ul><h1>Search Results</h1>';
 
   for (product of products) {
 
     if (value === undefined || product.name.toLowerCase().indexOf(value.toLowerCase()) !== -1) {
-
       html += '<article>';
-        html += '<div>'
-          html += '<div class="section">'
-              html += '<a href="' + product.image + '"rel="nofollow noopener noreferrer" target="_blank">'
-                html += '<h2>' + product.name + '</h2>'
-              html += '</a>'
-
-              html += '<ul>'
+      html += '<div>'
+      html += '<div class="section">'
+      html += '<a href="' + product.image + '"rel="nofollow noopener noreferrer" target="_blank">'
+      html += '<h2>' + product.name + '</h2>'
+      html += '</a>'
+      html += '<ul>'
       html += '<li><b>Status</b>: ' + product.status + '</li>'
       html += '<li><b>Gender</b>: ' + product.gender + '</li>'
       html += '<li><b>Species</b>: ' + product.species + '</li>'
-    html += '</ul>'
-
-          html += '</div>'
-
-
-
-        html += '</div>'
-        html += '</br>'
-        html += '<div>'
-          html += '<img src="'+ product.image + '" alt="'+ product.name + '">'
-
-        html += '</div>'
-
+      html += '</ul>'
+      html += '</div>'
+      html += '</div>'
+      html += '</br>'
+      html += '<div>'
+      html += '<img src="'+ product.image + '" alt="'+ product.name + '">'
+      html += '</div>'
       html += '</article>'
-
     }
-  }
 
-  html += '</ul>'
+  }
 
   document.getElementById("proposed_results").innerHTML =  html;
 }
